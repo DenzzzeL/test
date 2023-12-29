5a137cada58b4f5f0c1bc97af5d798221936237b
DenzzzeL <den_eezzz@mail.ru>
2023-12-29 22:00:48 +0600

    added lesson-03

---
+++ lesson-03/lesson-03.js
@@ -0,0 +1,6 @@
+let titleProject = 'Оценка стоимости'
+let screenValue = 'шаблонные, с уникальным дизайном, с анимациями'
+let screenPrice = 100
+let percentage = 10
+let fullPrice = 1000
+let responsive = true
